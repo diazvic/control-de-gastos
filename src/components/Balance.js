e1383ca96962429727d19771c6b1dc12f8997346
Victoria Diaz <diazmariavictoria94@gmail.com>
2022-06-07 20:57:11 -0300

    Armamos barra de navegación y rutas

---
+++ src/components/Balance.js
@@ -0,0 +1,9 @@
+const Balance = () => {
+	return (
+		<div>
+			<h1>BALANCE</h1>
+		</div>
+	);
+};
+
+export default Balance;
---
+++ src/components/Categorias.js
@@ -0,0 +1,10 @@
+const Categorias = () => {
+	return (
+		<div>
+			{" "}
+			<h1>CATEGORIAS</h1>
+		</div>
+	);
+};
+
+export default Categorias;
