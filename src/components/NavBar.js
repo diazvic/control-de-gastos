3523fb8e35fc34488a3457da1047162d7cf7b88d
Victoria Diaz <65260980+diazvic@users.noreply.github.com>
2024-07-16 09:53:49 +0200

    Agrego estilos a la barra de navegacion, variables en sass

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,38 @@
 import { Link } from "react-router-dom";
 import WalletIcon from "@mui/icons-material/Wallet";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import SellIcon from "@mui/icons-material/Sell";
+import PieChartIcon from "@mui/icons-material/PieChart";
+import "../styles/_NavBar.scss";
 const NavBar = () => {
 	return (
 		<nav>
-			<div>
-				<WalletIcon />
+			<div className="contenedor-nav-titulo">
+				<WalletIcon className="icono-nav-cartera" />
 				<h1>control de gastos</h1>
 			</div>
-			<ul>
-				<li>
-					<Link to="/Balance" />
-				</li>
-				<li>
-					<Link to="/Categorias" />
-				</li>
-				<li>
-					<Link to="/Reportes" />
-				</li>
-			</ul>
+			<div className="contenedor-nav-lista">
+				<ul>
+					<li>
+						<Link to="/Balance" className="link-nav">
+							<TrendingUpIcon className="iconos-nav" />
+							balance
+						</Link>
+					</li>
+					<li>
+						<Link to="/Categorias" className="link-nav">
+							<SellIcon className="iconos-nav" />
+							categorías
+						</Link>
+					</li>
+					<li>
+						<Link to="/Reportes" className="link-nav">
+							<PieChartIcon className="iconos-nav" />
+							reportes
+						</Link>
+					</li>
+				</ul>
+			</div>
 		</nav>
 	);
 };
